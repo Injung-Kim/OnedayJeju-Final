0130c6b608782d65250fa26d8f8b678ca37a92a4
HYELIM <hyerimm1127@gmail.com>
2021-06-09 17:48:50 +0900

    404, 500 에러 페이지 생성

---
+++ src/main/java/jeju/controller/ErrorController.java
@@ -0,0 +1,21 @@
+package jeju.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/error")
+public class ErrorController {
+	
+	@GetMapping("/server-error")
+	public String serverError() {
+		return "error/serverError";
+	}
+	
+	@GetMapping("/no-resource")
+	public String noResource() {
+		return "error/noResource";
+	}
+	
+}
