aa5a364e81d03e95799cf097719cb0d3216e1688
ParkTae <parktae004@gmail.com>
2021-06-17 21:11:31 +0900

    여행 후기 게시판 완

--- src/main/java/jeju/dao/face/RvCommentDao.java
+++ src/main/java/jeju/dao/face/RvCommentDao.java
@@ -7,13 +7,34 @@ import jeju.dto.RvComment;
 
 public interface RvCommentDao {
 
+	/**
+	 * 게시글 번호를 이용한 게시글 댓글 전체 객체
+	 * 
+	 * @param review - 게시글 번호를 이용한 객체
+	 * @return 게시글 댓글 리스트 반환
+	 */
 	public List<RvComment> selectComment(Review review);
 
+	/**
+	 * 게시글 댓글 추가
+	 * 
+	 * @param rvcomment - 게시글 댓글번호 담긴 객체
+	 */
 	public void insert(RvComment rvcomment);
 
 
+	/**
+	 * 게시글 댓글 수정
+	 * 
+	 * @param rvcomment - 게시글 댓글 번호 객체
+	 */
 	public void commentUpdateCommit(RvComment rvcomment);
 
+	/**
+	 * 게시글 댓글 삭제
+	 * 
+	 * @param rvcomment - 게시글 댓글 번호 객체
+	 */
 	public void commentCancel(RvComment rvcomment);
 
 	
